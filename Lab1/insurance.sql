22acd0746c55c5604fdc2411b327132988b2a666
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-06-14 12:58:48 +0530

    Update insurance.sql

--- Lab1/insurance.sql
+++ Lab1/insurance.sql
@@ -50,19 +50,19 @@ Values('1','Abc','Delhi'),
 COMMIT;
 
 INSERT INTO CAR
-Values('1111','Alto','2000'),
-('1234','i10','2003'),
-('1122','Omni','2005'),
-('2233','Suzuki','2008'),
-('3344','brezza','2004');
+Values('1111','Alto',2000),
+('1234','i10',2003),
+('1122','Omni',2005),
+('2233','Suzuki',2008),
+('3344','brezza',2004);
 COMMIT;
 
 INSERT INTO ACCIDENT
-Values('11','1-2-2000','Delhi'),
-('22','2-3-2003','Lucknow'),
-('33','4-2-2005','Kolkatha'),
-('44','5-8-2008','Kerala'),
-('55','7-11-2004','Bihar');
+Values(11,'2000-2-1','Delhi'),
+(22,'2003-3-2','Lucknow'),
+(33,'2005-2-4','Kolkatha'),
+(44,'2008-8-5','Kerala'),
+(55,'2004-11-4','Bihar');
 COMMIT;
 
 INSERT INTO OWNS
@@ -74,12 +74,23 @@ Values('1','1111'),
 COMMIT;
 
 INSERT INTO PARTICIPATED
-Values('1','1111','11','1000'),
-('2','1234','22','2000'),
-('5','1122','33','5000'),
-('3','2233','44','8000'),
-('7','3344','55','7000');
+Values('1','1111',11,1000),
+('2','1234',12,2000),
+('5','1122',13,5000),
+('3','2233',14,8000),
+('7','3344',15,7000);
 COMMIT;
 
 SELECT * FROM PERSON;
 SELECT * FROM CAR;
+SELECT * FROM OWNS;
+SELECT * FROM ACCIDENT;
+SELECT * FROM PARTICIPATED;
+
+UPDATE PARTICIPATED SET damage_amount=25000 WHERE report_number=12 and Regno='1234';
+
+INSERT INTO ACCIDENT Values(23,'2010-5-9','Mysore');
+INSERT INTO PARTICIPATED Values('4','1111',23,10000);
+
+SELECT COUNT(*) AS Totalcars FROM CAR C,PARTICIPATED P WHERE C.Regno = P.Regno AND C.model='Alto';
+
