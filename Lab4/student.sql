a73a97fbdf086e94a3a45590a95980b707b451b0
ansha001 <97624380+ansha001@users.noreply.github.com>
2022-07-12 23:40:40 +0530

    Update student.sql

--- Lab4/student.sql
+++ Lab4/student.sql
@@ -1,6 +1,6 @@
-DROP DATABASE student18;
-CREATE DATABASE student18;
-USE student18;
+DROP DATABASE student;
+CREATE DATABASE student;
+USE student;
 
 CREATE TABLE STUDENT (
 	snum int,
@@ -68,18 +68,18 @@ VALUES ('class1', '22/1/1 10:15:16' ,'R1', 14),
 ('class6', '22/1/1 13:20:20' ,'R2', 14),
 ('class7', '22/1/1 10:10:10' ,'R3', 14);
 
-/*  names of all Juniors (level = JR) who are enrolled in a class taught by fid 11*/
 
+
+/*  names of all Juniors (level = JR) who are enrolled in a class taught by fid 11*/
 SELECT S.sname
 FROM STUDENT S, CLASS C, ENROLLED E, FACULTY F
 WHERE F.fid = C.fid AND S.snum = E.snum AND C.cname=E.cname
- AND S.level = 'jr' AND F.fid=11;
+	AND S.level = 'jr' AND F.fid=11;
  
  /* names of all classes that either meet in room R128 or have five or more Students enrolled*/
- 
- SELECT C.cname
- FROM CLASS C
- WHERE C.room = 'R128' OR C.cname IN (
+SELECT C.cname
+FROM CLASS C
+WHERE C.room = 'R128' OR C.cname IN (
 	SELECT E.cname
     FROM ENROLLED E
 	GROUP BY(E.cname)
@@ -87,10 +87,60 @@ WHERE F.fid = C.fid AND S.snum = E.snum AND C.cname=E.cname
     );
 
 /* names of all students who are enrolled in two classes that meet at the same time.*/
-
 SELECT  DISTINCT S.sname
 FROM STUDENT S, CLASS C1, CLASS C2, ENROLLED E1, ENROLLED E2
-WHERE C1.meets_at = C2.meets_at AND E1.snum = E2.snum AND E1.snum = S.snum
-AND E1.cname=C1.cname AND E2.cname = C2.cname AND E1.cname <> E2.cname
+WHERE C1.meets_at = C2.meets_at AND 
+	E1.snum = E2.snum AND E1.snum = S.snum AND 
+    E1.cname = C1.cname AND E2.cname = C2.cname AND E1.cname <> E2.cname
 GROUP BY(C1.meets_at)
 HAVING COUNT(*)>1;
+
+/*Find the names of faculty members who teach in every room in which some class is taught*/
+SELECT DISTINCT F.fname
+FROM FACULTY F
+WHERE NOT EXISTS(
+	SELECT C.room
+    FROM CLASS C 
+    where C.room NOT IN (
+		SELECT c.room
+        FROM CLASS c
+        WHERE c.fid= F.fid
+        )
+	);
+    
+/*Find the names of faculty members for whom the combined enrolment of the courses that they teach is less
+than five*/
+SELECT DISTINCT F.fname
+FROM FACULTY F
+WHERE 5> (
+	SELECT COUNT(E.snum)
+    FROM CLASS C, ENROLLED E
+    WHERE C.cname=E.cname AND C.fid=F.fid
+    );
+    
+/*Find the names of students who are not enrolled in any class*/
+SELECT DISTINCT S.sname
+FROM STUDENT S
+WHERE S.snum NOT IN(
+	SELECT E.snum
+    FROM ENROLLED E
+    );
+    
+/*For each age value that appears in Students, find the level value that appears most often. For example, if
+there are more FR level students aged 18 than SR, JR, or SO students aged 18, you should print the pair (18,
+FR)*/
+SELECT S.age, S.level
+FROM STUDENT S
+GROUP BY S.age, S.level
+HAVING S.level IN (
+	SELECT S1.level
+    FROM STUDENT S1
+    WHERE S1.AGE = S.AGE
+    GROUP BY S1.level, S1.age
+    HAVING COUNT(*) >= ALL (
+		SELECT COUNT(*)
+        FROM STUDENT S2
+        WHERE S1.age = S2.age
+        GROUP BY S2.level, S2.age
+        )
+	);
